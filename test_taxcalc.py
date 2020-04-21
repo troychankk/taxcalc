ca519856d718936ac3d6ddf163a4051a94e2b222
302302agile <302cemcem@gmail.com>
2020-04-21 14:13:27 +0800

    updated_test_cases

--- test_taxcalc.py
+++ test_taxcalc.py
@@ -15,6 +15,18 @@ class TaxcalcTestCase(unittest.TestCase):
         result = Main.Tax_calc(joi_nci);
         self.assertEqual(expected, result);
 
+    def test_joint_800000_500000(self):
+        expected = 150000;
+        joi_nci = Main.Joint_deduc(800000, 500000);
+        result = Main.Tax_calc(joi_nci);
+        self.assertEqual(expected, result);
+
+    def test_joint_256000_275000(self):
+        expected = 22876.5;
+        joi_nci = Main.Joint_deduc(256000, 275000);
+        result = Main.Tax_calc(joi_nci);
+        self.assertEqual(expected, result);
+
     def test_single_50(self):
         expected = 0;
         sin_nci = Main.Single_deduc(50);
@@ -51,9 +63,21 @@ class TaxcalcTestCase(unittest.TestCase):
         result = Main.Tax_calc(joi_nci);
         self.assertEqual(expected, result);
 
+    def test_joint_603422_538743(self):
+        expected = 125168.05;
+        joi_nci = Main.Joint_deduc(603422, 538743);
+        result = Main.Tax_calc(joi_nci);
+        self.assertEqual(expected, result);
+
     def test_joint_5000000000_120000(self):
         expected = 750014400;
         joi_nci = Main.Joint_deduc(5000000000, 120000);
+        result = Main.Tax_calc(joi_nci);
+        self.assertEqual(expected, result);
+
+    def test_joint_5401000_3470000(self):
+        expected = 1325250;
+        joi_nci = Main.Joint_deduc(5401000, 3470000);
         result = Main.Tax_calc(joi_nci);
         self.assertEqual(expected, result);
 
@@ -63,6 +87,11 @@ class TaxcalcTestCase(unittest.TestCase):
         result = Main.Tax_calc(joi_nci);
         self.assertEqual(expected, result);
 
+    def test_joint_39708990_34535530(self):
+        expected = 11131278;
+        joi_nci = Main.Joint_deduc(39708990, 34535530);
+        result = Main.Tax_calc(joi_nci);
+        self.assertEqual(expected, result);
 
 unittest.TestLoader().loadTestsFromTestCase(TaxcalcTestCase)
 
